5d9dc6fb0ef9feaa180945c53f34f15c9709f737
DamodharGona <gonadamodharreddy999@gmail.com>
2026-08-19 22:04:45 +0530

    fixed url

--- job-tracker-frontend/src/repository/authApis.js
+++ job-tracker-frontend/src/repository/authApis.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api";
+const API_BASE_URL = (import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api")
+  .replace(/\/api\/?$/, "") + "/api";
 
 export const loginUser = async ({ email, password }) => {
   try {
